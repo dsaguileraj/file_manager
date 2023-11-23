62efbdb4dc7992540f3371fa35ee8e492c041581
Diego Aguilera <dsaguileraj@gmail.com>
2023-11-23 12:16:44 -0500

    management app

--- apps/management/models.py
+++ apps/management/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=True)
+    url = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
