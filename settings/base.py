da7fe1baa344cf216cdc72bd564fb8ba5ea99b56
Diego Aguilera <dsaguileraj@gmail.com>
2023-11-23 19:19:20 -0500

    change settings

--- settings/base.py
+++ settings/base.py
@@ -42,6 +42,7 @@ CUSTOM_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 
@@ -60,10 +61,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'file_manager.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
