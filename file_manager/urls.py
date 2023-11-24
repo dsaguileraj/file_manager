20f3991722c7b77bacf15899b0e9e695adbd3e26
Diego Aguilera <dsaguileraj@gmail.com>
2023-11-23 22:33:56 -0500

    manage urls

--- file_manager/urls.py
+++ file_manager/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import redirect
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include("web_views.routes")),
+    path("", lambda request: redirect("login", permanent=True)),
     path("admin/", admin.site.urls),
     path("api/", include("api.routes")),
 ]
